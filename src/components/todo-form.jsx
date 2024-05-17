573748b07a75f178e7ec20ea6e3ff589d1f68f87
yeeunseo-dev <anfrhrl96@gmail.com>
2024-05-17 12:31:19 +0900

    refactor:백업

--- src/components/todo-form.jsx
+++ src/components/todo-form.jsx
@@ -27,20 +27,26 @@ function TodoForm({ onSubmit }) {
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="할 일을 추가하세요"
-        value={title}
-        className="todo-title"
-        onChange={handleTitleChange}
-      />
-      <input
-        type="text"
-        placeholder="자세한 일정을 적어주세요"
-        value={content}
-        className="todo-content"
-        onChange={handleContentChange}
-      />
+      <label htmlFor="todo-title">
+        제목
+        <input
+          type="text"
+          placeholder="할 일을 추가하세요"
+          value={title}
+          className="todo-title"
+          onChange={handleTitleChange}
+        />
+      </label>
+      <label htmlFor="todo-content">
+        내용
+        <input
+          type="text"
+          placeholder="자세한 일정을 적어주세요"
+          value={content}
+          className="todo-content"
+          onChange={handleContentChange}
+        />
+      </label>
       <button type="submit" className="todo-button">
         추가
       </button>
